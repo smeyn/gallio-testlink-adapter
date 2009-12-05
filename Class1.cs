f50b522d645230439ca914ca0b3eb7d6af12b83a
StephanMeyn <StephanMeyn@568ac0c2-1038-11de-aa17-9100d24c02a7>
2009-12-05 00:52:20 +0000

    NUnit Adapter unit tests

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Meyn.TestLink;
+
+namespace nunitTests
+{
+    [TestFixture]
+    [TestLinkFixture(
+    Url = "http://localhost/testlink/lib/api/xmlrpc.php",
+    ProjectName = "FMFS",
+    UserId = "admin",
+    TestPlan = "Automatic Testing",
+    TestSuite = "nunitAddOnSampleTests",
+    DevKey = "b6e8fee35d143cd018d3b683e0777c51")]
+    public class Class1Tests
+    {
+        [Test]
+        public void FailThis()
+        {
+            Assert.Fail("Failed because it had to");
+        }
+        [Test]
+        public void Succeed()
+        {
+        }
+    }
+}
